f8db5078dc5915c7074078ca453834d85b5941e3
Naveen Yerravarapu <naveen.yerravarapu@modak.com>
2025-01-31 10:32:59 +0530

    intial commit - build and publish docker image

---
+++ app/SimpleTimeServiceapp.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def get_time():
+    return jsonify({
+        "timestamp": datetime.utcnow(),
+        "ip": request.remote_addr
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=18630)
